f7cda0c21cb989a477c0a6af5c555feb2545f882
Johan Cortes <jako12287@gmail.com>
2024-11-11 06:47:17 -0500

    cors v4

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use("/api", orderRoutes);
